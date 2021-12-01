8f7ffcc37123e4e767b32b1e0d80f45e6992388f
Isabelle <isabelle.ceugnart@gmail.com>
2021-12-01 13:57:56 +0100

    feature : Affichage Patient

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
-
+from controller.patientcontroller import patientController
+from model.patient import Patient
 
 app = Flask(__name__)
+patient=Patient()
+patientcontroller = patientController()
+
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/inscription")
-def inscription():
-    return render_template("addpatient.html")
+
+@app.route("/displayPatient")
+def affichage():
+    return patientcontroller.fetch_patient(patient)
