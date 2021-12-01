00e66d293f24b32809ebe7906902178eeb64b75a
ryigitoglu <93184810+ryigitoglu@users.noreply.github.com>
2021-12-01 11:11:52 +0100

    ajout des pages html et route

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/inscription")
-def inscription():
+@app.route("/addpat")
+def addPatient():
     return render_template("addpatient.html")
+
+@app.route("/addinf")
+def addInfirmier():
+    return render_template("addInfirmier.html")
+
+@app.route("/adddep")
+def addDeplacement():
+    return render_template("addDeplacement.html")
+
+
+@app.route("/delpa")
+def deletePatient():
+    return render_template("deletePatient.html")
+
+@app.route("/delinf")
+def deleteInfirmier():
+    return render_template("deleteInfirmier.html")
